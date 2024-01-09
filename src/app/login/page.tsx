3998eea7801b1bd1f3f80d2d16524ff524328b66
neo@zillinks.com <keunhey0107@gmail.com>
2024-01-09 20:44:04 +0900

    Update: github login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,26 @@
 'use client';
 
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
+
 
 const Login = () => {
-  console.log('asdad');
-  console.log(process.env.NEXT_PUBLIC_GITHUB_ID);
+
+
   const loginWithGithub = () => {
     window.location.assign(`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_ID}`);
   };
+
+  useEffect(() => {
+    // http://localhost:3000/login?code=6b6f673a05846ee0f5dd
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const codeParam = urlParams.get("code");
+    console.log(codeParam);
+
+  }, []);
+
+
   return (
     <button onClick={loginWithGithub}>Login</button>
   );
