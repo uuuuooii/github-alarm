beda1022d0e1e83de45ee79c59febbc024c21cef
neo@zillinks.com <keunhey0107@gmail.com>
2024-01-14 15:59:10 +0900

    Update: 오늘의 커밋 수 찾기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   // TODO: 전체 commit 수 가져오기
 
   const getData = async () => {
-    const token = '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';
+    const token = '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';
     const config = {
       headers: {
         Authorization: `Bearer ${token}`
@@ -21,26 +21,29 @@ export default function Home() {
     };
     try {
       // 유저 정보 가져오기
-      const userResponse = await axios.get(`https://api.github.com/users/${username}`);
-      const user = userResponse.data;
+      // const userResponse = await axios.get(`https://api.github.com/users/${username}`);
+      // const user = userResponse.data;
       // console.log(userResponse);
 
       // 레포지토리 정보 가져오기
-      const reposResponse = await axios.get(`https://api.github.com/users/${username}/repos`);
+      const reposResponse = await axios.get(`https://api.github.com/users/${username}/repos`, config);
       const repos = reposResponse.data;
       // console.log(repos);
 
-      const today = new Date().toLocaleDateString(); // 현재 날짜를 문자열로 변환
-      let todayCommits = 0;
+
+      let commitCount = 0;
 
       for (let i = 0; i < repos.length; i++) {
         const repo = repos[i];
 
         // 레포지토리별 커밋 정보 가져오기
-        const commitsResponse = await axios.get(`https://api.github.com/repos/${username}/${repo.name}/commits`);
+        const commitsResponse = await axios.get(`https://api.github.com/repos/${username}/${repo.name}/commits`, config);
         const commitData = commitsResponse.data;
 
-
+        if (commitData.length === 0) {
+          console.log(`저장소에 커밋이 없음: ${repo.name}`);
+          continue; // 다음 저장소로 건너뛰기
+        }
         // 오늘 날짜의 커밋 필터링
         const todayCommits = commitData.filter((commit: { commit: { author: { date: string | number | Date; }; }; }) => {
           const commitDate = new Date(commit.commit.author.date).toLocaleDateString();
@@ -48,8 +51,12 @@ export default function Home() {
           return commitDate === today;
         });
 
+
+        commitCount += todayCommits.length;
         // 레포지토리명과 오늘의 커밋 수 출력
         console.log(`레포지토리: ${repo.name}, 오늘 커밋: ${todayCommits.length}`);
+        console.log(` 오늘 커밋 수: ${commitCount}`);
+
       }
 
 
