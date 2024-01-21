0ed65b7e582c02ceaccaee18f7fd793e299ba393
neo@zillinks.com <keunhey0107@gmail.com>
2024-01-21 18:48:30 +0900

    Fix: cors 에러

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `https://github-alarm.vercel.app/:path*`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
