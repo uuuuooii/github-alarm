df9d14fabb54fd078ebb159e55a5db9b2c7027d7
neo@zillinks.com <keunhey0107@gmail.com>
2024-01-25 23:16:23 +0900

    Update: 흐름 추가

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,14 @@
 import mysql from 'mysql2/promise';
 
-export async function query({ query, values = [] }) {
+interface QueryParams {
+  query: string;
+  values?: any[];
+}
+
+export const query = async ({ query, values = [] }: QueryParams) => {
   const dbconnection = await mysql.createConnection({
     host: process.env.MYSQL_HOST,
-    post: process.env.MYSQL_PORT,
+    port: parseInt(process.env.MYSQL_PORT || ''),
     database: process.env.MYSQL_DATABASE,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
@@ -13,8 +18,7 @@ export async function query({ query, values = [] }) {
     const [results] = await dbconnection.execute(query, values);
     dbconnection.end();
     return results;
-  } catch (error) {
+  } catch (error: any) {
     throw Error(error.message);
-    return { error };
   }
-}
+};
