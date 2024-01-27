27f900851244642d5e5a51655f0934c39cba9a98
neo@zillinks.com <keunhey0107@gmail.com>
2024-01-27 18:48:17 +0900

    Update: point API 제작 중

--- src/app/api/point/route.ts
+++ src/app/api/point/route.ts
@@ -5,15 +5,56 @@
 
 import { NextResponse, NextRequest } from 'next/server';
 import { query } from '../../../lib/db';
+import axios from 'axios';
 
 export const GET = async (request: NextResponse, response: NextResponse) => {
   try {
-    let commit = 1;
+    let commitCount = 0;
     let continuoDays = 3;
     let point = 0;
 
+    // 레포지토리 정보 가져오기
+    const reposResponse = await fetch(
+      `https://api.github.com/users/${'uuuuooii'}/repos`
+    );
+    const repos = await reposResponse.json();
+    console.log('repos', repos);
+
+    for (let i = 0; i < repos.length; i++) {
+      const repo = repos[i];
+
+      // 레포지토리별 커밋 정보 가져오기
+      const commitsResponse = await axios.get(
+        `https://api.github.com/repos/${'uuuuooii'}/${repo.name}/commits`
+      );
+      const commitData = commitsResponse.data;
+      console.log('commitData', commitData);
+      if (commitData.length === 0) {
+        console.log(`저장소에 커밋이 없음: ${repo.name}`);
+        continue; // 다음 저장소로 건너뛰기
+      }
+      // 오늘 날짜의 커밋 필터링
+      const todayCommits = commitData.filter(
+        (commit: { commit: { author: { date: string | number | Date } } }) => {
+          const commitDate = new Date(
+            commit.commit.author.date
+          ).toLocaleDateString();
+          const today = new Date().toLocaleDateString();
+          return commitDate === today;
+        }
+      );
+
+      commitCount += Number(todayCommits.length);
+
+      // 레포지토리명과 오늘의 커밋 수 출력
+      console.log(
+        `레포지토리: ${repo.name}, 오늘 커밋: ${todayCommits.length}`
+      );
+      console.log(` 오늘 커밋 수: ${commitCount}`);
+    }
+
     // 하루 커밋 수가 1개 이상이면 4포인트 부여
-    if (commit >= 1) {
+    if (commitCount >= 1) {
       point += 4;
     }
 
@@ -38,7 +79,7 @@ export const GET = async (request: NextResponse, response: NextResponse) => {
     }
 
     return new NextResponse(point, { status: 200 });
-  } catch {
+  } catch (error) {
     console.log(error);
     return new NextResponse('Internal Server Error', { status: 500 });
   }
