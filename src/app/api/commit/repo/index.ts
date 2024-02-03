54234771d4ceef15d3eb47157b6aad7fc0044870
neo@zillinks.com <keunhey0107@gmail.com>
2024-02-03 20:34:35 +0900

    Update: commit api 수정

---
+++ src/app/api/commit/repo/index.ts
@@ -0,0 +1,70 @@
+import { query } from '@/lib/db';
+import axios from 'axios';
+import { NextResponse } from 'next/server';
+
+export const getRepoData = async (
+  authorization: string | NextResponse<unknown>,
+  json: any
+) => {
+  const userId = 97392254; // 사용자 ID
+
+  // 레포 정보
+  const reposResponse = await fetch(
+    `https://api.github.com/users/${json.login}/repos`,
+    {
+      method: 'GET',
+      headers: {
+        Authorization: String(authorization),
+      },
+    }
+  );
+
+  const repos = await reposResponse.json();
+
+  let commitCount = 0;
+  let today;
+
+  for (let i = 0; i < repos.length; i++) {
+    const repo = repos[i];
+
+    // 레포지토리별 커밋 정보 가져오기
+    const commitsResponse = await axios.get(
+      `https://api.github.com/repos/${json.login}/${repo.name}/commits`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: String(authorization),
+        },
+      }
+    );
+    const commitData = commitsResponse.data;
+
+    if (commitData.length === 0) {
+      console.log(`저장소에 커밋이 없음: ${repo.name}`);
+      continue; // 다음 저장소로 건너뛰기
+    }
+
+    // 오늘 날짜의 커밋 필터링
+    const todayCommits = commitData.filter(
+      (commit: { commit: { author: { date: string | number | Date } } }) => {
+        const commitDate = new Date(
+          commit.commit.author.date
+        ).toLocaleDateString();
+        today = new Date().toLocaleDateString();
+
+        return commitDate === today;
+      }
+    );
+
+    commitCount += todayCommits.length;
+    // 레포지토리명과 오늘의 커밋 수 출력
+    console.log(`레포지토리: ${repo.name}, 오늘 커밋: ${todayCommits.length}`);
+    console.log(` 오늘 커밋 수: ${commitCount}`);
+  }
+
+  // DB 저장
+  await query({
+    query: 'INSERT INTO commit (id, commit_day, commit_count) VALUES (?, ?, ?)',
+    values: [userId, today, commitCount],
+  });
+};
--- src/app/api/commit/route.ts
+++ src/app/api/commit/route.ts
@@ -4,6 +4,12 @@ import { NextResponse, NextRequest } from 'next/server';
 import { query } from '@/lib/db';
 import axios from 'axios';
 import { authorizationHeader } from '@/lib/utils/authorization';
+import { getRepoData } from './repo';
+
+interface commitDataProps {
+  commit_day: Date;
+  commit_count: number;
+}
 
 export const GET = async (request: NextRequest, response: NextResponse) => {
   try {
@@ -24,73 +30,29 @@ export const GET = async (request: NextRequest, response: NextResponse) => {
     const json = await data.json();
 
     // 레포 정보
-    const reposResponse = await fetch(
-      `https://api.github.com/users/${json.login}/repos`,
+    await getRepoData(authorization, json);
+
+    // DB 전체 커밋 정보
+    const commitData = (await query({
+      query: 'SELECT * FROM commit WHERE id = ?',
+      values: [userId],
+    })) as commitDataProps[];
+
+    const formattedCommitData = commitData.map((commit) => ({
+      commit_day: commit.commit_day,
+      commit_count: commit.commit_count,
+    }));
+    const length = formattedCommitData.length;
+
+    return new NextResponse(
+      JSON.stringify({ data: formattedCommitData, length }),
       {
-        method: 'GET',
+        status: 200,
         headers: {
-          Authorization: String(authorization),
+          'Access-Control-Allow-Origin': '*',
         },
       }
     );
-
-    const repos = await reposResponse.json();
-    let commitCount = 0;
-    let today;
-
-    for (let i = 0; i < repos.length; i++) {
-      const repo = repos[i];
-
-      // 레포지토리별 커밋 정보 가져오기
-      const commitsResponse = await axios.get(
-        `https://api.github.com/repos/${json.login}/${repo.name}/commits`,
-        {
-          method: 'GET',
-          headers: {
-            Authorization: String(authorization),
-          },
-        }
-      );
-      const commitData = commitsResponse.data;
-
-      if (commitData.length === 0) {
-        console.log(`저장소에 커밋이 없음: ${repo.name}`);
-        continue; // 다음 저장소로 건너뛰기
-      }
-
-      // 오늘 날짜의 커밋 필터링
-      const todayCommits = commitData.filter(
-        (commit: { commit: { author: { date: string | number | Date } } }) => {
-          const commitDate = new Date(
-            commit.commit.author.date
-          ).toLocaleDateString();
-          today = new Date().toLocaleDateString();
-
-          return commitDate === today;
-        }
-      );
-
-      commitCount += todayCommits.length;
-      // 레포지토리명과 오늘의 커밋 수 출력
-      console.log(
-        `레포지토리: ${repo.name}, 오늘 커밋: ${todayCommits.length}`
-      );
-      console.log(` 오늘 커밋 수: ${commitCount}`);
-    }
-
-    // DB 저장
-    await query({
-      query:
-        'INSERT INTO commit (id, commit_day, commit_count) VALUES (?, ?, ?)',
-      values: [userId, today, commitCount],
-    });
-
-    return new NextResponse(JSON.stringify(json), {
-      status: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-      },
-    });
   } catch (error) {
     console.log(error);
     return new NextResponse('Internal Server Error', { status: 500 });
