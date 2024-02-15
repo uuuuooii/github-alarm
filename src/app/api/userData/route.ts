7d065e305f74bd3dd6f9957efb688f0e97552d29
neo@zillinks.com <keunhey0107@gmail.com>
2024-02-15 22:06:42 +0900

    Fix: 빌드에러 확인

--- src/app/api/userData/route.ts
+++ src/app/api/userData/route.ts
@@ -5,16 +5,16 @@ import { updatePoint } from './update/point';
 import { updateConsecutiveDay } from './update/consecutiveDay';
 import { authorizationHeader } from '@/lib/utils/authorization';
 
-// interface ResultProps {
-//   id: number;
-//   name: string;
-//   nickname: string;
-//   nth_day: number;
-//   all_point: number;
-//   max_consecutive_days: number;
-// }
+interface ResultProps {
+  id: number;
+  name: string;
+  nickname: string;
+  nth_day: number;
+  all_point: number;
+  max_consecutive_days: number;
+}
 
-export const GET = async (request, response) => {
+export const GET = async (request: NextRequest, response: NextResponse) => {
   try {
     // Authorization 헤더를 가져오기
     const authorization = await authorizationHeader(request);
@@ -28,10 +28,10 @@ export const GET = async (request, response) => {
     const json = await data.json();
 
     // 커밋 기록 조회
-    const resultUser = await query({
+    const resultUser = (await query({
       query: 'SELECT * FROM users WHERE id = ?',
       values: [json.id],
-    });
+    })) as ResultProps[];
 
     const findId = resultUser.find((item) => item.id === Number(json.id));
 
@@ -44,7 +44,7 @@ export const GET = async (request, response) => {
     }
 
     // 며칠 째인지 업데이트
-    const nthDay = await updateNthDay(Number(json.id));
+    // const nthDay = await updateNthDay(Number(json.id));
 
     // point 업데이트
     const pointData = await updatePoint(Number(json.id));
@@ -57,7 +57,7 @@ export const GET = async (request, response) => {
       id: json.id,
       name: json.name,
       nickname: json.login,
-      nth_day: nthDay.nth_day,
+      // nth_day: nthDay.nth_day,
       all_point: pointData.all_point,
       max_consecutive_days: maxDay.max_consecutive_days,
     };
--- src/app/api/userData/update/nthDay.ts
+++ src/app/api/userData/update/nthDay.ts
@@ -9,44 +9,39 @@ interface ResultPops {
 export const updateNthDay = async (userId: number) => {
   let day = 0;
 
-  try {
-    const result = (await query({
-      query: 'SELECT created_at, updated_at FROM users WHERE id = ?',
-      values: [userId],
-    })) as ResultPops[];
-    console.log(result);
-    const createDay = new Date(result[0].created_at);
-    const updateDay = new Date(result[0].updated_at);
+  const result = (await query({
+    query: 'SELECT created_at, updated_at FROM users WHERE id = ?',
+    values: [userId],
+  })) as ResultPops[];
+  console.log(result);
+  const createDay = new Date(result[0].created_at);
+  const updateDay = new Date(result[0].updated_at);
 
-    const formattedCreateDate = createDay.toLocaleDateString('ko-KR', {
-      timeZone: 'UTC',
-    });
-    const formattedUpdateDate = updateDay.toLocaleDateString('ko-KR', {
-      timeZone: 'UTC',
-    });
+  const formattedCreateDate = createDay.toLocaleDateString('ko-KR', {
+    timeZone: 'UTC',
+  });
+  const formattedUpdateDate = updateDay.toLocaleDateString('ko-KR', {
+    timeZone: 'UTC',
+  });
 
-    console.log('formattedCreateDate', formattedCreateDate);
-    console.log('formattedUpdateDate', formattedUpdateDate);
+  console.log('formattedCreateDate', formattedCreateDate);
+  console.log('formattedUpdateDate', formattedUpdateDate);
 
-    const oldDate = new Date(formattedCreateDate);
-    const newDate = new Date(formattedUpdateDate);
+  const oldDate = new Date(formattedCreateDate);
+  const newDate = new Date(formattedUpdateDate);
 
-    let diff = Math.abs(newDate.getTime() - oldDate.getTime());
-    diff = Math.ceil(diff / (1000 * 60 * 60 * 24));
-    console.log('diff', diff);
-    if (diff == 0) {
-      day++;
-    } else {
-      day = diff + 1;
-    }
-    await query({
-      query: 'UPDATE users SET nth_day = ?  WHERE id = ?',
-      values: [day, userId],
-    });
-
-    return { nth_day: day };
-  } catch (error) {
-    console.log(error);
-    return new NextResponse(JSON.stringify(error), { status: 500 });
+  let diff = Math.abs(newDate.getTime() - oldDate.getTime());
+  diff = Math.ceil(diff / (1000 * 60 * 60 * 24));
+  console.log('diff', diff);
+  if (diff == 0) {
+    day++;
+  } else {
+    day = diff + 1;
   }
+  await query({
+    query: 'UPDATE users SET nth_day = ?  WHERE id = ?',
+    values: [day, userId],
+  });
+
+  return { nth_day: day };
 };
